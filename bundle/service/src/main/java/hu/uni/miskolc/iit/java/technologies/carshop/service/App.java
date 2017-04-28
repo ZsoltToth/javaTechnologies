4d2f7ad38333274b1bac998c3683aaf39da49310
Paszinczki, <dora.paszinczki@misys.com>
2017-04-28 13:53:19 +0200

    Cars Shop project structure has been defined.
    api, service, persist, console modules have been added to bundle.

---
+++ bundle/service/src/main/java/hu/uni/miskolc/iit/java/technologies/carshop/service/App.java
@@ -0,0 +1,13 @@
+package hu.uni.miskolc.iit.java.technologies.carshop.service;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
