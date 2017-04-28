7fb1a04d24b22d7011c079effbedda109413d115
Zsolt Toth <tothzs87@gmail.com>
2017-04-28 14:47:07 +0200

    Car Shop API - model classes and service interfaces have been defined

---
+++ bundle/api/src/main/java/hu/uni/miskolc/iit/java/technologies/carshop/api/service/CarManagementService.java
@@ -0,0 +1,11 @@
+package hu.uni.miskolc.iit.java.technologies.carshop.api.service;
+
+import java.util.Collection;
+
+import hu.uni.miskolc.iit.java.technologies.carshop.api.model.Car;
+
+public interface CarManagementService {
+
+	Collection<Car> listCars();
+	void acquireCar(Car car);
+}
